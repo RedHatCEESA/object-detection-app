ee6c3bc6a95382376e3b5e2ce7a9d5b71d894cdc
piomin <piotr.minkowski@gmail.com>
2023-04-21 15:02:46 +0200

    new logo (2)

--- frontend/src/Register/components/terms.js
+++ frontend/src/Register/components/terms.js
@@ -1,34 +1,25 @@
 const text= `
 REGULAMIN KONKURSU
-„zbierAI punkty, wygrywaj nagrody”
+„Zbieraj punkty, wygrywaj nagrody”
 
 § 1
 Postanowienia ogólne
 
-1. Organizatorem konkursu pod nazwą „zbierAI punkty, wygrywaj nagrody” określanego dalej jako
-„Konkurs” i przyrzekającym nagrody w konkursie są :
-ESKOM IT Sp. z o.o. z siedzibą, W Warszawie przy ul. Puławskiej 543 02-844 Warszawa, NIP
-5252458209, REGON 1419465, KRS 0000332007 oraz
-DCS Data Center Solutions Sp. z o.o. z siedzibą w Krakowie przy Al. Zygmunta Krasińskiego 14/3a
-30-101 Kraków, NIP: 6762554993, REGON 381359556, KRS 0000749749
-Dalej zwanym „organizatorzy”.
+1. Organizatorem konkursu pod nazwą „Zbieraj punkty, wygrywaj nagrody” określanego dalej jako
+„Konkurs” i przyrzekającym nagrody w konkursie jest:
+NTT Poland Sp. z o.o. z siedzibą, W Warszawie przy ul.Sienna 73, 00-833 Warszawa, NIP
+9521851834 | KRS 0000063801 | REGON 016422763
 2. Konkurs odbywa się zgodnie z zasadami określonymi w niniejszym regulaminie (dalej:
 „Regulamin”) oraz zgodnie z przepisami polskiego prawa.
-3. Konkurs jest przeprowadzany w okresie dnia 09.11.2022 roku w godzinach 11:00 – 16:30 na
-terenie: Warsaw Hilton Hotel – Red Hat Summit Connect Warszawa 2022.
-4. Celem Konkursu jest nagrodzenie osób, które wykonały określone w ramach Konkursu zadanie,
-na zasadach i warunkach określonych w niniejszym Regulaminie.
+3. Konkurs jest przeprowadzany w dniach 25-26 kwietnia 2023 roku w godzinach 11:00 – 13:30 na terenie: Targów Kielce – POLSECURE 2023.
+4. Celem Konkursu jest nagrodzenie osób, które wykonały określone w ramach Konkursu zadanie, na zasadach i warunkach określonych w niniejszym Regulaminie.
 
 § 2
 Uczestnicy Konkursu
 1. Udział w Konkursie mogą wziąć wyłącznie osoby fizyczne.
-2. Udziału w Konkursie nie mogą brać członkowie organów ani pracownicy Organizatora lub firm na
-których polecenie działa Organizator, ani inne osoby zaangażowane w organizację Konkursu, a
-także ich małżonkowie, wstępni, zstępni, rodzeństwo oraz osoby pozostające w stosunku
-przysposobienia.
+2. Udziału w Konkursie nie mogą brać członkowie organów ani pracownicy Organizatora Konkursu lub firm na których polecenie działa Organizator Konkursu, ani inne osoby zaangażowane w organizację Konkursu, a także ich małżonkowie, wstępni, zstępni, rodzeństwo oraz osoby pozostające w stosunku przysposobienia.
 3. Osoba zgłaszająca udział w Konkursie może zgłosić wyłącznie swój własny udział.
-4. Osoba, która spełnia warunki udziału w Konkursie oraz zgłosi udział w Konkursie zgodnie z
-Regulaminem zwana jest dalej „Uczestnikiem”, a wszystkie takie osoby łącznie „Uczestnikami”.
+4. Osoba, która spełnia warunki udziału w Konkursie oraz zgłosi udział w Konkursie zgodnie z Regulaminem zwana jest dalej „Uczestnikiem”, a wszystkie takie osoby łącznie „Uczestnikami”.
 5. Jeden Uczestnik może dokonać tylko jednego zgłoszenia w Konkursie.
 
 § 3
@@ -37,80 +28,50 @@ Zasady i warunki udziału w Konkursie
 
 1. W celu udziału w Konkursie należy, w terminie trwania Konkursu określonym w § 1 ust. 3
 Regulaminu:
-a) Zarejestrować się na stronie konkursowej podanej przez organizatora. Warunkiem udziału w
-konkursie jest rejestracja przy pomocy adresu e-mail użytego przy rejestracji uczestnika
-konferencji.
+a) Zarejestrować się na stronie konkursowej podanej przez organizatora. Warunkiem udziału w konkursie jest rejestracja przy pomocy adresu e-mail.
 b) wykonać zadanie konkursowe, opisane w ust. 2 poniżej;
-
-c) podać wymagane dane osobowe oraz złożyć oświadczenia dotyczące akceptacji warunków
-Regulaminu oraz wyrażenia zgody na przetwarzanie danych osobowych;
-2. Zadanie polega na skanowaniu przedmiotów fizycznych celem rozpoznania przez aplikację
-udostępnioną przez organizatora (dalej: „Odpowiedź”).
+c) podać wymagane dane osobowe oraz złożyć oświadczenia dotyczące akceptacji warunków Regulaminu oraz wyrażenia zgody na przetwarzanie danych osobowych;
+2. Zadanie polega na skanowaniu przedmiotów fizycznych celem rozpoznania przez aplikację udostępnioną przez Organizatora Konkursu (dalej: „Odpowiedź”).
 
 § 4
 Wybór zwycięzców
 
-1. Organizator powołuje komisję konkursową (dalej: „Komisja”), składającą się z nieparzystej liczby
-osób (nie mniej niż 3), która przenalizuje zdobytą przez uczestników liczbę punktów.
+1. Organizator powołuje komisję konkursową (dalej: „Komisja”), składającą się z nieparzystej liczby osób (nie mniej niż 3), która przeanalizuje zdobytą przez uczestników liczbę punktów.
 
 § 5
 Nagrody
 
-1. Dla trzech uczestników o największej zdobytej liczbie punktów (dalej „Nagroda”) przewidziane
-są nagrody niespodzianki zapewnione przez organizatora.
-2. W przypadkach w których obowiązujące prawo przewiduje opłacenie podatku dochodowego w
-związku z nagrodą, do nagrody, o której mowa w ust. 1 powyżej Organizator przyznaje również
-nagrodę pieniężną w kwocie stanowiącej równowartość 11,11% wartości tej nagrody. Za zgodą
-Zwycięzcy ta nagroda pieniężna odpowiadająca wysokości podatku dochodowego obciążającego
-Zwycięzcę zostanie pobrana i wpłacona na rachunek właściwego urzędu skarbowego przez
-Organizatora, jako płatnika zryczałtowanego podatku dochodowego od osób fizycznych od
+1. Dla trzech uczestników o największej zdobytej liczbie punktów (dalej „Nagroda”) przewidziane są nagrody niespodzianki zapewnione przez Organizatora Konkursu.
+2. W przypadkach, w których obowiązujące prawo przewiduje opłacenie podatku dochodowego w związku z nagrodą, do nagrody, o której mowa w ust. 1 powyżej Organizator Konkursu przyznaje również nagrodę pieniężną w kwocie stanowiącej równowartość 11,11% wartości tej nagrody. Za zgodą Zwycięzcy ta nagroda pieniężna odpowiadająca wysokości podatku dochodowego obciążającego Zwycięzcę zostanie pobrana i wpłacona na rachunek właściwego urzędu skarbowego przez Organizatora Konkursu, jako płatnika zryczałtowanego podatku dochodowego od osób fizycznych od
 wygranej w Konkursie.
-3. Jeśli Zwycięzca nie jest płatnikiem podatku dochodowego w Polsce, warunkiem otrzymania
-nagrody jest udostępnienie Organizatorowi informacji niezbędnych do uregulowania przez
-Organizatora należności podatkowych, względem organów podatkowych właściwych dla
-rezydencji podatkowej Zwycięzcy.
-4. Nagroda, o której mowa w ust. 1 powyżej wraz z nagrodą pieniężną, o której mowa w ust. 2 i 3
-powyżej, zwana jest dalej łącznie „Nagrodą”.
+3. Jeśli Zwycięzca nie jest płatnikiem podatku dochodowego w Polsce, warunkiem otrzymania nagrody jest udostępnienie Organizatorowi Konkursu informacji niezbędnych do uregulowania przez Organizatora należności podatkowych, względem organów podatkowych właściwych dla rezydencji podatkowej Zwycięzcy.
+4. Nagroda, o której mowa w ust. 1 powyżej wraz z nagrodą pieniężną, o której mowa w ust. 2 i 3 powyżej, zwana jest dalej łącznie „Nagrodą”.
 5. Nagroda zostanie przekazana Zwycięzcy niezwłocznie po ogłoszeniu wyników konkursu.
-6. Nagroda nie podlega zamianie na ekwiwalent pieniężny ani na inne świadczenia rzeczowe w tym
-również na inną nagrodę.
+6. Nagroda nie podlega zamianie na ekwiwalent pieniężny ani na inne świadczenia rzeczowe w tym również na inną nagrodę.
 
 § 6
 
 Nadzór nad przestrzeganiem Regulaminu
 
-Nad przestrzeganiem Regulaminu czuwa Komisja. W przypadku niespełnienia wymogów uczestnictwa
-określonych w Regulaminie lub jeżeli Odpowiedź nie spełnia wymaganych standardów określonych w
-Regulaminie lub w przypadku innego rażącego naruszenia postanowień Regulaminu, Komisja
-decyduje o wykluczeniu danego Uczestnika z udziału w Konkursie, a Uczestnik traci prawo do
-otrzymania Nagrody.
+Nad przestrzeganiem Regulaminu czuwa Komisja. W przypadku niespełnienia wymogów uczestnictwa określonych w Regulaminie lub jeżeli Odpowiedź nie spełnia wymaganych standardów określonych w Regulaminie lub w przypadku innego rażącego naruszenia postanowień Regulaminu, Komisja decyduje o wykluczeniu danego Uczestnika z udziału w Konkursie, a Uczestnik traci prawo do otrzymania Nagrody.
 
 § 7
 Reklamacje
 
-1. Bez ograniczeń przewidzianych przepisami powszechnie obowiązującego prawa uprawnień
-przysługujących konsumentowi w zakresie zgłaszania i dochodzenia roszczeń, można zgłaszać do
-Organizatora wszelkie reklamacje dotyczące danego Konkursu.
-2. W celu usprawnienia procesu zgłaszania i rozpatrywania, reklamacja może zostać przesłana
-listem poleconym na adres Organizatora wskazany w § 1 ust. 1 Regulaminu lub na adres e-mail
-piotr.zakrzewski@eskom.eu. Dla ułatwienia identyfikacji osoby zgłaszającej reklamację oraz
+1. Bez ograniczeń przewidzianych przepisami powszechnie obowiązującego prawa uprawnień przysługujących konsumentowi w zakresie zgłaszania i dochodzenia roszczeń, można zgłaszać do Organizatora Konkursu wszelkie reklamacje dotyczące danego Konkursu.
+2. W celu usprawnienia procesu zgłaszania i rozpatrywania, reklamacja może zostać przesłana listem poleconym na adres Organizatora Konkursu wskazany w § 1 ust. 1 Regulaminu lub na adres e-mail aleksandra.porowska@global.ntt. Dla ułatwienia identyfikacji osoby zgłaszającej reklamację oraz
 przedmiotu reklamacji rekomendowane jest, by reklamacja określała w szczególności imię,
-nazwisko, adres zgłaszającego, a także dokładny opis oraz wskazanie przyczyny reklamacji, jak
-również określenie żądania.
-3. Reklamacja, o której mowa powyżej, powinna zostać zgłoszona w terminie 14 (czternastu) dni od
-wykrycia przez zgłaszającego okoliczności stanowiącej podstawę tej reklamacji.
-4. Organizator zobowiązuje się udzielić odpowiedzi na reklamację w terminie 14 (czternastu) dni od
-jej otrzymania. Organizator może przesłać odpowiedź listem poleconym lub w formie
-wiadomości e-mail (jeżeli zgłaszający wskazał swój adres e-mail).
+nazwisko, adres zgłaszającego, a także dokładny opis oraz wskazanie przyczyny reklamacji, jak również określenie żądania.
+3. Reklamacja, o której mowa powyżej, powinna zostać zgłoszona w terminie 14 (czternastu) dni od wykrycia przez zgłaszającego okoliczności stanowiącej podstawę tej reklamacji.
+4. Organizator zobowiązuje się udzielić odpowiedzi na reklamację w terminie 14 (czternastu) dni od jej otrzymania. Organizator Konkursu może przesłać odpowiedź listem poleconym lub w formie wiadomości e-mail (jeżeli zgłaszający wskazał swój adres e-mail).
 
 § 8
 Dane osobowe
 
 Administratorem danych osobowych w związku z przeprowadzeniem Konkursu jest
-ESKOM IT Sp. z o.o. z siedzibą, W Warszawie przy ul. Puławskiej 543 02-844 Warszawa, NIP
-5252458209, REGON 1419465, KRS 0000332007 oraz
-DCS Data Center Solutions Sp. z o.o. z siedzibą w Krakowie przy Al. Zygmunta Krasińskiego 14/3a
-30-101 Kraków, NIP: 6762554993, REGON 381359556, KRS 0000749749
+NTT Poland Sp. z o.o. z siedzibą, W Warszawie przy ul.Sienna 73, 00-833 Warszawa, NIP
+9521851834 | KRS 0000063801 | REGON 016422763
+
 1. Dane osobowe są przetwarzane:
 1) w oparciu o odpowiednią zgodę wyrażoną przez osobę, której dane dotyczą, w celu
 przeprowadzenia Konkursu, wyłonienia Zwycięzcy, wydania Nagrody, kontaktowania się w
@@ -133,36 +94,19 @@ przedprocesowych, procesowych oraz egzekucyjnych, zmierzających do zaspokojenia
 roszczeń lub obrony Administratora; dane są przetwarzane do upływu okresu przedawnienia
 roszczeń.
 
-2. Administrator może podpowierzyć dane osobowe Organizatorowi, a także innym podmiotom
-świadczącym na jego rzecz usługi wymagające przetwarzania danych, w tym usługi reklamowe,
-usługi księgowe, usługi prawne, usługi kurierskie, pocztowe oraz usługi IT. Dane osobowe mogą
-być udostępnianie podmiotom do tego upoważnionym na podstawie przepisów prawa.
+2. Administrator może podpowierzyć dane osobowe Organizatorowi Konkursu, a także innym podmiotom świadczącym na jego rzecz usługi wymagające przetwarzania danych, w tym usługi reklamowe, usługi księgowe, usługi prawne, usługi kurierskie, pocztowe oraz usługi IT. Dane osobowe mogą być udostępnianie podmiotom do tego upoważnionym na podstawie przepisów prawa.
 3. Administrator informuje, że osoby, których dane dotyczą mają prawo: dostępu do swoich
-danych osobowych, uzyskania ich kopii, sprostowania, usunięcia, ograniczenia przetwarzania,
-przeniesienia danych.
-4. Osoba, której dane osobowe są przetwarzane na podstawie jej zgody ma prawo do cofnięcia
-zgody w dowolnym momencie, z zastrzeżeniem, że cofnięcie zgody nie wpływa na zgodność z
-prawem przetwarzania, którego dokonano na podstawie zgody przed jej cofnięciem.
-5. Osoba, której dane dotyczą ma prawo w dowolnym momencie, z przyczyn związanych z jej
-szczególną sytuacją, wnieść sprzeciw wobec przetwarzania dotyczących jej danych osobowych,
-opartego o podstawę wskazaną w ust. 2 pkt 3) powyżej.
-6. Oświadczenia w zakresie wykonywania praw przysługujących osobie, której dane dotyczą,
-określonych w ust. 4 - 6 powyżej, należy kierować na adres e-mail: piotr.zakrzewski@eskom.eu
-7. Osoba, której dane dotyczą ma prawo wniesienia skargi w zakresie ochrony danych osobowych
-do organu nadzorczego, tj. Prezesa Urzędu Ochrony Danych Osobowych.
-8. Podanie danych osobowych oraz wyrażenie zgody na przetwarzanie danych osobowych w celu
-przeprowadzenia Konkursu jest dobrowolne, jednak konieczne do wzięcia udziału w Konkursie.
-Administrator informuje ponadto, że niepodanie określonych danych uniemożliwi w
-szczególności wybór Zwycięzcy, kontaktowanie się z Uczestnikami, wydanie Nagrody, realizację
-uprawnień reklamacyjnych, czy realizację obowiązków podatkowych Administratora,
-wynikających z przepisów prawa.
+danych osobowych, uzyskania ich kopii, sprostowania, usunięcia, ograniczenia przetwarzania, przeniesienia danych.
+4. Osoba, której dane osobowe są przetwarzane na podstawie jej zgody ma prawo do cofnięcia zgody w dowolnym momencie, z zastrzeżeniem, że cofnięcie zgody nie wpływa na zgodność z prawem przetwarzania, którego dokonano na podstawie zgody przed jej cofnięciem.
+5. Osoba, której dane dotyczą ma prawo w dowolnym momencie, z przyczyn związanych z jej szczególną sytuacją, wnieść sprzeciw wobec przetwarzania dotyczących jej danych osobowych, opartego o podstawę wskazaną w ust. 2 pkt 3) powyżej.
+6. Oświadczenia w zakresie wykonywania praw przysługujących osobie, której dane dotyczą, określonych w ust. 4 - 6 powyżej, należy kierować na adres e-mail: aleksandra.porowska@global.ntt
+7. Osoba, której dane dotyczą ma prawo wniesienia skargi w zakresie ochrony danych osobowych do organu nadzorczego, tj. Prezesa Urzędu Ochrony Danych Osobowych.
+8. Podanie danych osobowych oraz wyrażenie zgody na przetwarzanie danych osobowych w celu przeprowadzenia Konkursu jest dobrowolne, jednak konieczne do wzięcia udziału w Konkursie. Administrator informuje ponadto, że niepodanie określonych danych uniemożliwi w szczególności wybór Zwycięzcy, kontaktowanie się z Uczestnikami, wydanie Nagrody, realizację uprawnień reklamacyjnych, czy realizację obowiązków podatkowych Administratora, wynikających z przepisów prawa.
 
 § 9
 Postanowienia końcowe
 
-1. Regulamin dostępny jest w siedzibie Organizatora oraz w okresie trwania Wydarzeń w
-wyznaczonych strefach, a na żądanie zgłoszone Organizatorowi na adres:
-piotr.zakrzewski@eskom.eu przesyłany e-mailem.
+1. Regulamin dostępny jest w siedzibie Organizatora Konkursu oraz w okresie trwania Wydarzeń w wyznaczonych strefach, a na żądanie zgłoszone Organizatorowi Konkursu na adres: aleksandra.porowska@global.ntt przesyłany e-mailem.
 2. Nazwy poszczególnych postanowień Regulaminu mają charakter informacyjny.
 3. We wszystkich sprawach dotyczących Konkursu, a nieuregulowanych Regulaminem mają
 zastosowanie przepisy prawa polskiego.
